45ff5902a74fdf8944e1e651ef1359c77e70de41
durgeshBluethink <durgesh1.bluethink@gmail.com>
2023-06-06 11:45:44 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,9 @@
+// let num = 1
+// let num1 =3
+// let result = num / num1
+// console.log(result)
+let num = true
+let num1 =true
+let result = num / num1
+console.log(result)
+
---
+++ hello.js
@@ -0,0 +1,6 @@
+console.log("hello wolrd!");
+num=2+2;
+console.log(num)
+console.log(num+4)
+let username='durgesh bluethink'
+console.log(username)
